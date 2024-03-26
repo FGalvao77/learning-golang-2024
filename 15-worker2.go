819f2bb57a332d40d78b228cadaf061a57628521
FGalvao77 <fernando.galvao77@outlook.com>
2024-03-26 12:46:48 -0300

    day 3...

---
+++ 15-worker2.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+  "fmt"
+  "time"
+)
+
+func worker(workerId int, data chan int) {
+  for x := range data {
+    time.Sleep(time.Second)
+    fmt.Printf("Worker %d received %d\n", workerId, x)
+    }
+  }
+
+func main() {
+  channel := make(chan int)
+  go worker(1, channel)
+  go worker(2, channel)
+
+  for i := 0; i < 10; i++ {
+    channel <- i
+  }
+}
---
+++ 18-user.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type user struct {
+  ID int
+  Name string
+  Email string
+}
+
+func main() {
+  mux := http.NewServeMux()
+  mux.HandleFunc("/users", listUsersHandler)
+  mux.HandleFunc("POST /users", createUserHandler)
+  http.ListenAndServe(":8080", mux)
+}
+
+func listUsersHandler(w http.ResponseWriter, r *http.Request) {
+  db, err := sql.Open("sqlite3", "users.db") 
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+  defer db.Close()
+
+  rows, err := db.Query("SELECT * FROM users") 
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return 
+  }
+  defer rows.Close()
+
+  users := []*user{}
+  for rows.Next() {
+    var u user
+    if err := rows.Scan(&u.ID, &u.Name, &u.Email); err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      return 
+    }
+    users = append(users, &u) 
+  }
+  if err := json.NewEncoder(w).Encode(users); err != nil {
+    http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
+    return 
+  }
+}
+
+func createUserHandler(w http.ResponseWriter, r *http.Request) {
+  db, err := sql.Open("sqlite3", "users.db")
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+  defer db.Close()
+
+  var u user
+  if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
+    http.Error(w, "Erros decoding JSON", http.StatusBadRequest)
+    return
+  }
+
+  if _, err := db.Exec("INSERT INTO users (name, email) VALUES (?, ?)", u.Name, u.Email); err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+
+  w.WriteHeader(http.StatusCreated)
+}
+
